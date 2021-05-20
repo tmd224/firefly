3fbe9e5a9fac9ce041b683c1d32f5e7dc2edb24e
tmd224 <41244312+tmd224@users.noreply.github.com>
2021-05-20 13:04:18 -0400

    Added Source model framework.  Mostly untested, more work to be done

--- core/models.py
+++ core/models.py
@@ -35,12 +35,12 @@ class StatParam():
         def __repr__(self):
             return "{}-{}".format(self.name, self.nom_value)
 
-        def get_value(self, nom=True):
+        def get_value(self, mode='nom'):
             """
             Get the value of the parameter.
 
             Args:
-                nom (bool): Returns nominal value if True, otherwise returns random value with distribution
+                mode (str): [nom, mc]. Returns nominal value or random value within defined distribution
 
             Returns:
                 val (int, float): value
@@ -52,7 +52,7 @@ class BaseModel():
     """
     Base model class template.  All models should inherit from here
     """
-    def __init__(self, id:int, name:str, tags:dict=None, sub_system_id:int=None):
+    def __init__(self, id:int, name:str, tags:dict=None, sub_system_id:int=None, mode:str='nom'):
         """
         Args:
             id (int): id number for internal use only
@@ -60,12 +60,14 @@ class BaseModel():
             tags (dict): Dictionary of tags which determine what states the load should be enabled or disabled.  The format of the dictionary
                 is 2 keys: 'enabled' and 'disabled'.  The values for both keys are lists of 'tag' strings.             
             sub_system_id (int): Sub-system id that the model belongs to
+            mode (str): Simulation mode: nom (nominal values), mc (monte carlo)
         """
         self.logger = logging.getLogger(self.__class__.__name__)
         self.id = validate_arg(id, int)
         self.name = validate_arg(name, str)
         self.sub_system_id = validate_arg(sub_system_id, int, optional=True)
-        self.tags = validate_arg(tags, dict, optional=True)          
+        self.tags = validate_arg(tags, dict, optional=True)
+        self.mode = validate_arg(mode, str, ['nom', 'mc'])       
 
         self.logger.debug("Creating new model object: {}".format(self.name))
 
@@ -74,7 +76,7 @@ class BaseLoad(BaseModel):
     Base load class
     """
 
-    def __init__(self, id:int, name:str, load_value:StatParam, input_resistance:StatParam=None, tags:dict=None, sub_system_id:str=None, ):
+    def __init__(self, id:int, name:str, load_value:StatParam, input_resistance:StatParam=None, tags:dict=None, sub_system_id:str=None, mode:str='nom'):
         """
         Args:
             id (int): id number for internal use only
@@ -84,8 +86,9 @@ class BaseLoad(BaseModel):
                 is 2 keys: 'enabled' and 'disabled'.  The values for both keys are lists of 'tag' strings.
             input_resistance (StatParam): input resistance of the load (i.e a series component prior to the Vcc pin)
             load_value (StatParam): load value, typically current, but units based on child class implementation
+            mode (str): Simulation mode: nom (nominal values), mc (monte carlo)
         """
-        super().__init__(id, name, tags, sub_system_id)
+        super().__init__(id, name, tags, sub_system_id, mode)
         self.load_value = validate_arg(load_value, StatParam)      
         self.input_resistance = validate_arg(input_resistance, StatParam, optional=True, default=StatParam('input_resistance', 'Ohm', 0.0))
         
@@ -153,6 +156,186 @@ class LoadSwitch(BaseModel):
         """
         self.child_loads[load_obj.id] = load_obj
         self.logger.debug("Adding load [{}-{}] to LoadSwitch [{}-{}]".format(load_obj.id, load_obj.name, self.id, self.name))
+
+class BaseSource(BaseModel):
+    """
+    BaseSource model
+    """
+    def __init__(self, id:int, name:str, vin:StatParam, vout:StatParam, max_current:StatParam, input_resistance:StatParam=None, output_resistance:StatParam=None, tags:dict=None, sub_system_id:str=None, mode:str='nom'):
+        """
+        Args:
+            id (int): id number for internal use only
+            name (str): display name for the load
+            vin (StatParam): Source voltage input
+            vout (StatParam): Regulated source voltage output
+            max_current (StatParam): Maximum output current at rated voltage
+            input_resistance (StatParam): input resistance of the source (i.e a series component prior to the input pin)
+            output_resistance (StatParam): output resistance of the source (i.e a series component at to the Vout pin)
+            sub_system (str): Sub-system name that the load belongs to
+            tags (dict): Dictionary of tags which determine what states the load should be enabled or disabled.  The format of the dictionary
+                is 2 keys: 'enabled' and 'disabled'.  The values for both keys are lists of 'tag' strings.
+            mode (str): Simulation mode: nom (nominal values), mc (monte carlo)
+        """
+        super().__init__(id, name, tags, sub_system_id, mode)
+        self.vin = validate_arg(vin, StatParam)
+        self.vout = validate_arg(vout, StatParam)
+        self.max_current = validate_arg(max_current, StatParam)
+
+        # optional arg defaults
+        default_input = StatParam('input_resistance', 'Ohm', 0)
+        default_output = StatParam('output_resistance', 'Ohm', 0)
+
+        self.input_resistance = validate_arg(input_resistance, StatParam, optional=True, default=default_input)
+        self.output_resistance = validate_arg(output_resistance, StatParam, optional=True, default=default_output)
+
+        # attributes
+        self.loads = list()     # loads that are fed from this voltage source
+        self.total_current = float()    # total load current supplied by the source
+        self.power_dissipation = float()    # attribute to hold power dissipated into this source
+
+    def calc_efficiency(self, current=None):
+        """
+        Calculate the efficiency of the source based on the load current.  If the current if NoneType, the method will use the
+        stored total load current attribute
+
+        Args:
+            current (float): load current in Amps
+
+        Returns:
+            eff (float): Converter efficiency in decimal form
+        """
+
+
+
+class EfficiencyModel():
+    """
+    Class to model the dynamic efficiency of a power converter (i.e. efficiency as a function of load current)
+    """
+    def __init__(self, efficiency:list, current:list=None):
+        """
+        This model can be configured in 2 different ways.  A simple singular efficiency value can be passed in
+        and current can be NoneType.  In this mode, all load currents will resolve to the same efficiency.
+        The 2nd way is to pass in a list of currents and associated efficiency values.  In this mode, the source efficiency
+        will be a function of source load current.  The efficiency values must map 1:1 to the list of current values.
+
+        Args:                        
+            efficiency (float, list): list of floats that represent the efficiency in decimal form (ie 0.1 = 10%)
+            current (list): list of floats that represent current in Amps
+        """
+        self.efficiency = validate_arg(efficiency, [float, list])
+        self.current = validate_arg(current, list, optional=True)
+
+    def get_eff(self, current=None):
+        """
+        Get the efficiency value for a particular load current
+
+        Args:
+            current (float): Load current in Amps
+
+        Returns:
+            eff (float): Efficiency value in decimal form
+        """
+        if type(self.efficiency) is float:
+            # efficiency is defined as single value, so current doens't matter
+            return self.efficiency
+        else:
+            current = validate_arg(current, [float, int])   # at this point current arg is no longer optional
+            # TODO use linear interpolation to find the closest efficiency value to the requested current value
+            return eff
+
+
+class SMPSSource(BaseSource):
+    """
+    This class models any type of Switch-Mode power supply (Buck, Boost, BuckBoost, SEPIC, etc)
+    """
+    def __init__(self, id:int, name:str, vin:StatParam, vout:StatParam, max_current:StatParam, input_resistance:StatParam=None, output_resistance:StatParam=None, tags:dict=None, sub_system_id:str=None, efficiency:EfficiencyModel=None):
+        """
+        Args:
+            id (int): id number for internal use only
+            name (str): display name for the load
+            vin (StatParam): Source voltage input
+            vout (StatParam): Regulated source voltage output
+            max_current (StatParam): Maximum output current at rated voltage
+            input_resistance (StatParam): input resistance of the source (i.e a series component prior to the input pin)
+            output_resistance (StatParam): output resistance of the source (i.e a series component at to the Vout pin)
+            sub_system (str): Sub-system name that the load belongs to
+            tags (dict): Dictionary of tags which determine what states the load should be enabled or disabled.  The format of the dictionary
+                is 2 keys: 'enabled' and 'disabled'.  The values for both keys are lists of 'tag' strings.
+            efficiency (EfficiencyModel): Efficiency object for this converter
+        """
+        super().__init__(int, name, vin, vout, max_current, input_resistance, output_resistance, tags, sub_system_id)
+        self.efficiency = validate_arg(efficiency, EfficiencyModel)
+
+class CapDividerSource(BaseSource):
+    """
+    Class to model a Capacitive Divider source
+    """
+def __init__(self, id:int, name:str, vin:StatParam, divider:int, max_current:StatParam, input_resistance:StatParam=None, output_resistance:StatParam=None, tags:dict=None, sub_system_id:str=None, efficiency:EfficiencyModel=None):
+        """
+        Args:
+            id (int): id number for internal use only
+            name (str): display name for the load
+            vin (StatParam): Source voltage input
+            divider (int): Division ratio of the source (output = input / divider)
+            max_current (StatParam): Maximum output current at rated voltage
+            input_resistance (StatParam): input resistance of the source (i.e a series component prior to the input pin)
+            output_resistance (StatParam): output resistance of the source (i.e a series component at to the Vout pin)
+            sub_system (str): Sub-system name that the load belongs to
+            tags (dict): Dictionary of tags which determine what states the load should be enabled or disabled.  The format of the dictionary
+                is 2 keys: 'enabled' and 'disabled'.  The values for both keys are lists of 'tag' strings.
+            efficiency (EfficiencyModel): Efficiency object for this converter
+        """
+        # first, calculate the nominal voltage output to pass into the base model 
+        _vin = validate_arg(vin, StatParam)
+        self.divider = validate_arg(divider, int)
+        vout = _vin / float(self.divider)
+        super().__init__(int, name, vin, vout, max_current, input_resistance, output_resistance, tags, sub_system_id)
+        self.efficiency = validate_arg(efficiency, EfficiencyModel)
+
+class LinearSource(BaseSource):
+    """
+    Class to model a linear voltage regulator
+    """
+    def __init__(self, id:int, name:str, vin:StatParam, vout:StatParam, max_current:StatParam, input_resistance:StatParam=None, output_resistance:StatParam=None, tags:dict=None, sub_system_id:str=None, vdropout:StatParam=None, iq:StatParam=None):
+            """
+            Args:
+                id (int): id number for internal use only
+                name (str): display name for the load
+                vin (StatParam): Source voltage input
+                vout (StatParam): Regulated source voltage output
+                max_current (StatParam): Maximum output current at rated voltage
+                input_resistance (StatParam): input resistance of the source (i.e a series component prior to the input pin)
+                output_resistance (StatParam): output resistance of the source (i.e a series component at to the Vout pin)
+                sub_system (str): Sub-system name that the load belongs to
+                tags (dict): Dictionary of tags which determine what states the load should be enabled or disabled.  The format of the dictionary
+                    is 2 keys: 'enabled' and 'disabled'.  The values for both keys are lists of 'tag' strings.
+                vdropout (StatParam): Dropout voltage of the regulator in volts
+                iq (StatParam): Quiescent current of the regulator in microvolts (uV)
+            """
+            super().__init__(int, name, vin, vout, max_current, input_resistance, output_resistance, tags, sub_system_id)
+            self.vdroput = validate_arg(vdropout, StatParam, optional=True, default=StatParam('Vdroput', 'V', 0))
+            self.iq = validate_arg(iq, StatParam, optional=True, default=StatParam('iq', 'uA', 0))
+            self.efficiency = None
+
+    def calc_efficiency(self, current=None):
+        """
+        Calculate the efficiency of the source based on the load current.  If the current if NoneType, the method will use the
+        stored total load current attribute
+
+        Args:
+            current (float): load current in Amps
+
+        Returns:
+            eff (float): Converter efficiency in decimal form
+        """
+        if current is None:
+            current = self.total_current
+
+        vin = self.vin.get_value(self.mode)
+        iq = self.iq.get_value(self.mode)
+        eff = (vin * current) / (vin * (current + iq))
+
+        return eff
 
 
 if __name__=="__main__":
